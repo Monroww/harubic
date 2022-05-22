6bfe8e38300f4e20e195c2a506bc50680781fa4d
Herdi Apriansyah <laplacedemon9@gmail.com>
2022-05-22 13:17:49 +0700

    membuat search dan tooltip

--- js/script.js
+++ js/script.js
@@ -23,9 +23,49 @@ $(document).ready(function() {
         } else {
             nav.fadeOut().removeClass('fixed');
         }
-        lastScrollTop = st;
+        lastScrollTop = st
     })
 
+    // tooltip
+    $(window).on('mouseenter', function(e) {
+        if (e.target.matches('.tooltip-trigger')) {
+            // showTooltip(e)
+            //     // console.log($(e.target).pageX);
+            $(".tooltip-trigger").bind({
+                mousemove: changeTooltipPosition(e),
+                mouseenter: showTooltip(e, e.target),
+                mouseleave: hideTooltip
+            });
+        } else {
+            hideTooltip()
+        }
+    })
+
+    $(window).click(function(e) {
+        // search
+        if (e.target.matches('#search-trigger')) {
+            $('#searchModal').fadeIn()
+        } else if (!e.target.id == "searchSection" || !$(e.target).parents("#searchSection").length) {
+            $('#searchModal').fadeOut()
+        }
+    })
+
+    function changeTooltipPosition(event) {
+        var tooltipX = event.pageX - 12;
+        var tooltipY = event.pageY + 18;
+        $('div.tooltip').css({ top: tooltipY, left: tooltipX });
+    };
+
+    function showTooltip(event, data) {
+        $('div.tooltip').remove();
+        $('<div class="tooltip absolute z-50 px-2 py-1 bg-dark rounded text-white text-sm">' + $(data).attr('data-tooltip') + '</div>')
+            .appendTo('body');
+        changeTooltipPosition(event);
+    };
+
+    function hideTooltip() {
+        $('div.tooltip').remove();
+    };
 
 
 
